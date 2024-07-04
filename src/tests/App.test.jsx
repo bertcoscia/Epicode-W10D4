1bd69f975f41002e56b1c582ce8bd153461b6b45
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-07-04 16:16:26 +0200

    more examples for q4

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -37,4 +37,22 @@ describe("Checks search bar", () => {
     const filteredBooks = await screen.findAllByRole("listItem");
     expect(filteredBooks).toHaveLength(6);
   });
+
+  it("return 2 elements if 'd&d' is typed in the input field", async () => {
+    render(<BookList />);
+    const inputField = screen.getByPlaceholderText("Search");
+    const user = userEvent.setup();
+    await user.type(inputField, "d&d");
+    const filteredBooks = await screen.findAllByRole("listItem");
+    expect(filteredBooks).toHaveLength(2);
+  });
+
+  it("return 0 elements if 'gerry scotti' is typed in the input field", async () => {
+    render(<BookList />);
+    const inputField = screen.getByPlaceholderText("Search");
+    const user = userEvent.setup();
+    await user.type(inputField, "gerry scotti");
+    const filteredBooks = await screen.queryAllByRole("listItem");
+    expect(filteredBooks).toHaveLength(0);
+  });
 });
